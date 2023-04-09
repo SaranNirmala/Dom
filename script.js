026eccc7b2f04780eb766c91b405dc1d5effe321
SaranNirmala <126362188+SaranNirmala@users.noreply.github.com>
2023-04-09 13:21:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+// var para=document.createElement("p");
+// para.innerHTML="this is Paragraph";
+// document.body.append(para);
+
+
+// var container=document.createElement("div");
+// container.setAttribute("class" ,"container");
+
+// var row=document.createElement("div");
+// row.setAttribute("class" ,"row");
+
+// var col=document.createElement("div");
+// col.innerHTML="This is Column";
+// col.setAttribute("class" ,"col");
+
+
+// row.append(col);
+// container.append(row);
+// document.body.append(container);
+
+
+function foo()
+{
+    var form=document.getElementById("fN").value;
+    var form1=document.getElementById("mN").value;
+    var form2=document.getElementById("lN").value;
+    var form3=document.getElementById("email").value;
+    console.log(form);
+    console.log(form1);
+    console.log(form2);
+    console.log(form3);
+}
+
+
